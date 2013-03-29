afabf585ffe27e61ec2b114f62c05f191638935f
Cameron <cameron@soycow.org>
2013-03-28 19:48:22 -0700

    Added support for A, B and Select buttons.

--- soundkeys/soundkeys.ino
+++ soundkeys/soundkeys.ino
@@ -7,7 +7,6 @@ void setup()
 {
   ADMUX |= (1 << REFS1) | (1 << REFS0);
   analogReference(INTERNAL);
-  Serial.begin(9600);
   //Keyboard.begin();
   pinMode(MIC_L, INPUT);
   pinMode(MIC_R, INPUT);
@@ -16,18 +15,53 @@ void setup()
 }
 
 char dpad[] = {'w','a', 's', 'd'};
+char select_btn = 'z';
+char b_btn = 'x';
+char a_btn = 'c';
+int micLread, micRread;
 
 void loop()
 {
-  Keyboard.releaseAll();
 
   if (digitalRead(ON_SWITCH) == 1) {
     digitalWrite(BOARD_LED, HIGH);
-    Keyboard.press(dpad[(analogRead(MIC_L) + analogRead(MIC_R) % 4)]);
+    for(int i = 0; i < sizeof(dpad); i++) {
+      Keyboard.release(dpad[i]);
+    }
+
+    int micLread = analogRead(MIC_L);
+    int micRread = analogRead(MIC_R); 
+
+    Keyboard.press(dpad[micLread + micRread % 4]);
+    press_mod_btn(micLread);
+    press_ab_btn(micRread);
   }
   else {
     digitalWrite(BOARD_LED, LOW);
+    Keyboard.releaseAll();
   }
 
   delay(500);
+}
+
+
+void press_mod_btn(int mic){ 
+  if (mic > 100) {
+    Keyboard.press(select_btn);
+  }
+  else {
+    Keyboard.release(select_btn);
+  }
+}
+
+void press_ab_btn(int mic){
+  if (mic % 2) {
+    Keyboard.release(b_btn);
+    Keyboard.press(a_btn);
+  }
+  else {
+    Keyboard.release(a_btn);
+    Keyboard.press(b_btn);
+  }
+
 }
